7bc30f213ed0a75967e686739822f1c06dff1477
Bahaa Abo Khaled <bahaa.love84@gmail.com>
2022-09-14 00:40:30 +0200

    Create Products Repo

---
+++ ShopApp-API/ShopApp-API/Services/IRepositry.cs
@@ -0,0 +1,10 @@
+﻿using ShopApp_API.Entiteis;
+
+namespace ShopApp_API.Services
+{
+    public interface IRepositry
+    {
+        List<Product> getProducts();
+        Product getProductById(int id); 
+    }
+}
---
+++ ShopApp-API/ShopApp-API/Services/Repositry.cs
@@ -0,0 +1,27 @@
+﻿using ShopApp_API.Data;
+using ShopApp_API.Entiteis;
+
+namespace ShopApp_API.Services
+{
+    public class Repositry : IRepositry
+    {
+        private readonly ShopContext context;
+
+        public Repositry(ShopContext context)
+        {
+            this.context = context;
+        }
+
+        public Product getProductById(int id)
+        {
+            var product = context.Products!.FirstOrDefault(p => p.Id == id);
+            return product!;
+        }
+
+        public List<Product> getProducts()
+        {
+            var products = context.Products!.ToList();
+            return products;
+        }
+    }
+}

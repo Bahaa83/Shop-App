b06d2bd93e8e539df2d598895079744e97545461
Bahaa Abo Khaled <bahaa.love84@gmail.com>
2022-09-14 13:34:05 +0200

    Fitch yest data  in products component

--- shopapp-client/src/Components/Products.js
+++ shopapp-client/src/Components/Products.js
@@ -1,15 +1,23 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import Product from "./Product";
 
 const Products = () => {
     const [products, setProducts] = useState([]);
     useEffect(() => {
         axios.get('https://localhost:5000/api/Products')
-            .then(resp => console.log(resp));
+            .then(resp => {setProducts(resp.data)
+            console.log(resp.data)})
+            .catch(err => console.log(err));
     }, [])
    
+    const showProducts = products.map(item =>
+        
+            <Product key={item.id} product={item} />
+    );
+    
     return (
-        <div>Products</div>
+        <div>{showProducts}</div>
     )
 }
 

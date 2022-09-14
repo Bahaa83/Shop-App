50abda07ea0ce15340a02063ca56ed0d8dc043bd
Bahaa Abo Khaled <bahaa.love84@gmail.com>
2022-09-14 10:36:00 +0200

    Remove the unnecessary files

---
+++ shopapp-client/src/ShopApp.js
@@ -0,0 +1,13 @@
+import './ShopApp.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Welcom in the Shop App
+      </header>
+    </div>
+  );
+}
+
+export default App;

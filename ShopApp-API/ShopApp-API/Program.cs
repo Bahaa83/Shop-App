56f7d7df7bde4e01315b2e82bb2a3ad1e14ccffb
Bahaa Abo Khaled <bahaa.love84@gmail.com>
2022-09-14 10:17:39 +0200

    Register the Irepo and Repo in Program file

--- ShopApp-API/ShopApp-API/Program.cs
+++ ShopApp-API/ShopApp-API/Program.cs
@@ -1,10 +1,11 @@
 using Microsoft.EntityFrameworkCore;
 using ShopApp_API.Data;
+using ShopApp_API.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddScoped<IRepositry, Repositry>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();

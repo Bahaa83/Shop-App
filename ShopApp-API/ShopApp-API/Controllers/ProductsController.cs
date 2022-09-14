24d465d71359cff6fdbe812ede8013503ed7397e
Bahaa Abo Khaled <bahaa.love84@gmail.com>
2022-09-14 10:18:24 +0200

    Create Producys controller and implement the Functions

---
+++ ShopApp-API/ShopApp-API/Controllers/ProductsController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ShopApp_API.Data;
+using ShopApp_API.Entiteis;
+using ShopApp_API.Services;
+
+namespace ShopApp_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IRepositry _repo;
+
+        public ProductsController(IRepositry repo)
+        {
+            this._repo = repo;
+        }
+        /// <summary>
+        /// Function to get all products
+        /// </summary>
+        /// <returns> list of product</returns>
+        [HttpGet]
+        public async Task< ActionResult<List<Product>>> getProducts()
+        {
+            return await _repo.getProducts();
+            
+        }
+        /// <summary>
+        /// Function to get product by id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns>Product opject</returns>
+        [HttpGet("{id}")]
+        public async Task< ActionResult<Product>> getProducById(int id)
+        {
+            return await _repo.getProductById(id);  
+           
+        }
+    }
+}

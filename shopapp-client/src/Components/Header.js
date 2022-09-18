cb5ef9c3d22c0c426fa8affa57739266a01965fe
Bahaa Abo Khaled <bahaa.love84@gmail.com>
2022-09-19 00:24:33 +0200

    Add links to appbar

--- shopapp-client/src/Components/Header.js
+++ shopapp-client/src/Components/Header.js
@@ -3,6 +3,9 @@ import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
+import { List, ListItem ,IconButton, Badge} from '@mui/material';
+import { NavLink } from 'react-router-dom';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 
 const pages = ['Home', 'Products', 'Contact'];
 const rightPages = ['Login', 'Register'];
@@ -16,11 +19,30 @@ const Header = () => {
   return (
     <AppBar position="fixed" >
       <Container maxWidth="xl">
-        <Toolbar disableGutters >
-          <Typography variant="h4" >
-           ShopApp
+        <Toolbar>
+          <Typography variant="h4" textTransform="uppercase">
+            ShopApp
           </Typography>
-         
+          <List sx={{display:"flex"}}>
+            {links.map(page => (
+              <ListItem component={NavLink} key={page} to={page} sx={{ color: "inherit",typography:"h7" }}  >
+                {page.toUpperCase()}
+              </ListItem>
+            ))}
+          </List>
+          <IconButton color="inherit" size='large' >
+            <Badge badgeContent="2" color="secondary">
+              <ShoppingCartIcon/>
+            </Badge>
+          </IconButton>
+          
+          <List sx={{display:"flex"}}>
+            {rightLinks.map(page => (
+              <ListItem component={NavLink} key={page} to={page} sx={{color:"inherit",typography:"h7"}}>
+                {page.toUpperCase()}
+              </ListItem>
+            ))}
+          </List>
         </Toolbar>
       </Container>
     </AppBar>

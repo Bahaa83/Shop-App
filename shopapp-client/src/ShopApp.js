ddec19ff2abb568d572edc41203651957d6a64b8
Bahaa Abo Khaled <bahaa.love84@gmail.com>
2022-09-18 15:53:03 +0200

    Import Routes and Route

--- shopapp-client/src/ShopApp.js
+++ shopapp-client/src/ShopApp.js
@@ -1,14 +1,23 @@
 import './ShopApp.css';
+import Home from "./Pages/Home";
+import { Fragment } from "react";
+import Header from "./Components/Header";
+import { Routes, Route } from "react-router-dom";
 import Products from "./Components/Products";
+import Contact from "./Pages/Contact";
+import ErrorPage from "./Pages/ErrorPage";
 
 function ShopApp() {
   return (
-    <div className="App">
-      <header className="App-header">
-        Welcom in the Shop App
-        <Products/>
-      </header>
-    </div>
+    <Fragment >
+      <Header />
+      <Routes>
+        <Route path='/home' element={<Home />}></Route>
+        <Route path='/products' element={<Products />}></Route>
+        <Route path='/contact' element={<Contact />}></Route>
+        <Route path='*' element={<ErrorPage/>}></Route>
+      </Routes>
+    </Fragment>
   );
 }
 

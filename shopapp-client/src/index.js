794b5c27326c00e45a01f0f4778a27a45de62772
Bahaa Abo Khaled <bahaa.love84@gmail.com>
2022-09-18 15:51:39 +0200

    Import BrowserRouter

--- shopapp-client/src/index.js
+++ shopapp-client/src/index.js
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import ShopApp from './ShopApp';
+import { BrowserRouter as Router } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <ShopApp />
+    <Router>
+      <ShopApp />
+    </Router>
   </React.StrictMode>
 );
 

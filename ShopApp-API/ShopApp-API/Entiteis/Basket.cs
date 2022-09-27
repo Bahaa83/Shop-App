81c9620b5a99150e514ef01a648dd32a4999cd69
Bahaa Abo Khaled <bahaa.love84@gmail.com>
2022-09-27 10:22:00 +0200

    Create Basket entity

---
+++ ShopApp-API/ShopApp-API/Entiteis/Basket.cs
@@ -0,0 +1,10 @@
+﻿namespace ShopApp_API.Entiteis
+{
+    public class Basket
+    {
+        public int Id { get; set; }
+        public string? BuyerId { get; set; }
+        public List<BasketItem> BasketItems { get; set; } = new();
+
+    }
+}

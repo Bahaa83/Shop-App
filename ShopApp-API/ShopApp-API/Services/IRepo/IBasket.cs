286f94259f0713478fbbd0b926966d63de72977b
Bahaa Abo Khaled <bahaa.love84@gmail.com>
2022-09-28 15:33:53 +0200

    Add createBasket  task

---
+++ ShopApp-API/ShopApp-API/Services/IRepo/IBasket.cs
@@ -0,0 +1,12 @@
+﻿using ShopApp_API.Entiteis;
+
+namespace ShopApp_API.Services.IRepo
+{
+    public interface IBasket
+    {
+        Task<Basket>GetBasket(string buyerId);
+        Task<Basket>CreateBasket(string buyerId);
+        Task<bool> AddBasketItem(int productId, int quantity);
+        Task<bool> RemoveBasketItem(int productId, int quantity);
+    }
+}

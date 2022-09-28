d4074f21b010c33b69b06f30e819bc7d08b0f764
Bahaa Abo Khaled <bahaa.love84@gmail.com>
2022-09-28 15:33:20 +0200

    implement AddItem() and RemoveItem() functions

--- ShopApp-API/ShopApp-API/Entiteis/Basket.cs
+++ ShopApp-API/ShopApp-API/Entiteis/Basket.cs
@@ -5,6 +5,28 @@
         public int Id { get; set; }
         public string? BuyerId { get; set; }
         public List<BasketItem> BasketItems { get; set; } = new();
+        public void AddBasketItem(Product product,int quantity)
+        {
+            // Check if this products is exist
+            if(BasketItems.All(x => x.Id == product.Id))
+            {
+                var existingItem = BasketItems.FirstOrDefault(x => x.Id == product.Id);
+                if(existingItem != null) existingItem.Quantity += quantity;
+            }
+            // Else
+            BasketItems.Add(new BasketItem { Product = product, Quantity = quantity });
+
+        }
+        public void RemoveBasketItem (int productId,int quantity)
+        {
+            var deletedItem = BasketItems.FirstOrDefault(x=> x.Id == productId);
+            if (deletedItem == null) return;
+            deletedItem.Quantity -= quantity;
+            if (deletedItem.Quantity == 0) BasketItems.Remove(deletedItem);
+                    
+            
+
+        }
 
     }
 }

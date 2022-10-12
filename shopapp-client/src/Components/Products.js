6b4da07bd6e3c2b8f5f47792c78c057e05623f63
Bahaa Abo Khaled <bahaa.love84@gmail.com>
2022-10-12 09:59:01 +0200

    Add loader to products component

--- shopapp-client/src/Components/Products.js
+++ shopapp-client/src/Components/Products.js
@@ -2,15 +2,18 @@ import React, { Fragment, useEffect, useState } from 'react';
 import axios from 'axios';
 import Product from "./Product";
 import Grid from '@mui/material/Grid';
+import Loader from "./Loader"
 
 
 const Products = () => {
     const [products, setProducts] = useState([]);
+     const [loading, setLoading] = useState(true);
     useEffect(() => {
-        axios.get('https://localhost:5000/api/Products')
+        axios.get('https://localhost:5000/api/Product')
             .then(resp => {setProducts(resp.data)
             console.log(resp.data)})
-            .catch(err => console.log(err));
+            .catch(err => console.log(err))
+        .finally(()=> setLoading(false))
     }, [])
    
     const showProducts = products.map(pro =>
@@ -20,7 +23,7 @@ const Products = () => {
             </Grid>
             
     );
-    
+    if(loading)return <Loader/>
     return (
         <Fragment>
             <Grid container rowSpacing={1} columnSpacing={3} sx={{mt:15,mb:5,px:2}}>
